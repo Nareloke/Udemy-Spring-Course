be7ce52e438a001065d08fd4c79a162a682a8c4b
Nareloke <lucas_anielo@hotmail.com>
2021-03-01 17:43:34 -0300

    CR: 4.23 - Spring Inversion Control

--- src/com/luv2code/springdemo/MyApp.java
+++ src/com/luv2code/springdemo/MyApp.java
@@ -4,7 +4,7 @@ public class MyApp {
 
 	public static void main(String[] args) {
 		// create the object
-		Coach theCoach = new BaseballCoach();
+		Coach theCoach = new TrackCoach();
 
 		// use the object
 		System.out.println(theCoach.getDailyWorkout());
