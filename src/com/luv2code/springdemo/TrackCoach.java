c1cc1c8ca2e67e494d1c96af2646390af79127e4
Nareloke <lucas_anielo@hotmail.com>
2021-03-06 15:50:48 -0300

    CR&UP: 6.48 - Bean Lifecycle

--- src/com/luv2code/springdemo/TrackCoach.java
+++ src/com/luv2code/springdemo/TrackCoach.java
@@ -22,5 +22,14 @@ public class TrackCoach implements Coach {
 	public String getDailyFortune() {
 		return "Just Do It: " + fortuneService.getFortune();
 	}
-
+	
+	// add an init method
+	public void doMyStartupStuff() {
+		System.out.println("Track Coach: inside method doMyStartupStuff");
+	}
+	
+	// add a destroy method
+	public void doMyCleanupStuffYoYo() {
+		System.out.println("Track Coach: inside method doMyCleanupStuffYoYo");
+	}
 }
