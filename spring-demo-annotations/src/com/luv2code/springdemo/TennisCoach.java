ebe6791048a45c50c98dc09299b1b98c8855e0a8
Nareloke <lucas_anielo@hotmail.com>
2021-03-07 15:31:53 -0300

    UP: 8.67 - Field Injection

--- spring-demo-annotations/src/com/luv2code/springdemo/TennisCoach.java
+++ spring-demo-annotations/src/com/luv2code/springdemo/TennisCoach.java
@@ -6,6 +6,7 @@ import org.springframework.stereotype.Component;
 @Component
 public class TennisCoach implements Coach {
 
+	@Autowired
 	private FortuneService fortuneService;
 
 	// define a default constructor
@@ -21,11 +22,13 @@ public class TennisCoach implements Coach {
 	}
 	*/
 	
+	/*
 	@Autowired
 	public void doSomeCrazyStuff(FortuneService fortuneService) {
 		System.out.println(">> TennisCoach: inside doSomeCrazyStuff() method");
 		this.fortuneService = fortuneService;
 	}
+	*/
 
 	@Override
 	public String getDailyWorkout() {
