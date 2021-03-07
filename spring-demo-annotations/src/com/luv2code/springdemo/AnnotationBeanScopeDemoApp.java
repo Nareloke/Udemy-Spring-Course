6a0966ab187b8e6be49c8687ea3f03ac4848838f
Nareloke <lucas_anielo@hotmail.com>
2021-03-07 18:32:26 -0300

    CR&UP: 9.77 - @Scope Annotation

---
+++ spring-demo-annotations/src/com/luv2code/springdemo/AnnotationBeanScopeDemoApp.java
@@ -0,0 +1,31 @@
+package com.luv2code.springdemo;
+
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class AnnotationBeanScopeDemoApp {
+
+	public static void main(String[] args) {
+		// load spring config file
+		ClassPathXmlApplicationContext context =
+				new ClassPathXmlApplicationContext("applicationContext.xml");
+		
+		// retrive bean from spring container
+		Coach theCoach = context.getBean("tennisCoach", Coach.class);
+		
+		Coach alphaCoach = context.getBean("tennisCoach", Coach.class);
+
+		// check if they are the same 
+		boolean result = (theCoach == alphaCoach);
+		
+		// print out the results
+		System.out.println("\nPointing to the same object: " + result);
+		
+		System.out.println("\nMemory location for theCoach: " + theCoach);
+		
+		System.out.println("\nMemory location for alphaCoach: " + alphaCoach);
+		
+		// close the context
+		context.close();
+	}
+
+}
