aaed78a9f5c4dfafec564f5842a65003ea5e6b45
Nareloke <lucas_anielo@hotmail.com>
2021-03-20 18:58:38 -0300

    CR&UP: 21.196 - Querying Objects with Hibernate I

--- hibernate-tutorial/src/com/luv2code/hibernate/demo/QueryStudentDemo.java
+++ hibernate-tutorial/src/com/luv2code/hibernate/demo/QueryStudentDemo.java
@@ -1,14 +1,12 @@
 package com.luv2code.hibernate.demo;
 
-import java.util.List;
-
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
 
 import com.luv2code.hibernate.demo.entity.Student;
 
-public class ReadStudentDemo {
+public class QueryStudentDemo {
 
 	public static void main(String[] args) {
 		
@@ -22,36 +20,25 @@ public class ReadStudentDemo {
 		Session session = factory.getCurrentSession();
 		
 		try {
+			// use the session object to save Java object
+			
+			// create a student object
+			System.out.println("Creating new student object...");
+			Student tempStudent = new Student("Paul", "Wall", "paul@luv2code.com");
 
 			// start a transaction
 			session.beginTransaction();
 
-			// query students
-			List<Student> theStudents = session.createQuery("from Student").getResultList();
+			// save the transaction
+			System.out.println("Saving the student...");
+			session.save(tempStudent);
 
-			// display the students
-			displayStudents(theStudents);
-
-			// query students: lastName = 'Doe'
-			theStudents = session.createQuery("from Student s where s.lastName='Doe'").getResultList();
-			
-			// display the students
-			System.out.println("\nStudents who have last name of Doe");
-			displayStudents(theStudents);
-
-			// commit the transaction
+			// commit transaction
 			session.getTransaction().commit();
-
 			System.out.println("Done!");
 
 		} finally {
 			factory.close();
-		}
-	}
-
-	private static void displayStudents(List<Student> theStudents) {
-		for (Student tempStudent : theStudents) {
-			System.out.println(tempStudent);
 		}
 	}
 
