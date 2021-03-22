d74d3779320f823accee7573eb04f00fb72aae67
Nareloke <lucas_anielo@hotmail.com>
2021-03-22 11:15:14 -0300

    UP: 25.228 - @OneToMany Bi-Directional Define Course Relationship

--- hb-03-one-to-many/src/com/luv2code/hibernate/demo/entity/Course.java
+++ hb-03-one-to-many/src/com/luv2code/hibernate/demo/entity/Course.java
@@ -1,6 +1,13 @@
 package com.luv2code.hibernate.demo.entity;
 
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 
 @Entity
@@ -17,10 +24,17 @@ public class Course {
 
 	// annotate fields
 
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	@Column(name="id")
 	private int id;
 
+	@Column(name="title")
 	private String title;
 
+	@ManyToOne(cascade= {CascadeType.PERSIST, CascadeType.MERGE,
+							CascadeType.DETACH, CascadeType.REFRESH})	
+	@JoinColumn(name="instructor_id")
 	private Instructor instructor;
 
 	public Course() {
