f430e4ea01b838697c2f5977e8ad45bac2cd0bb8
Nareloke <lucas_anielo@hotmail.com>
2021-03-22 17:11:47 -0300

    UP: 25.237 - Eager Vs Lazy Loading. Coding Lazy

--- hb-eagar-vs-lazy-demo/src/com/luv2code/hibernate/demo/entity/Instructor.java
+++ hb-eagar-vs-lazy-demo/src/com/luv2code/hibernate/demo/entity/Instructor.java
@@ -37,12 +37,12 @@ public class Instructor {
 	@JoinColumn(name="instructor_detail_id")
 	private InstructorDetail instructorDetail;
 
-	@OneToMany(fetch=FetchType.EAGER, 
+	@OneToMany(fetch=FetchType.LAZY, 
 			mappedBy="instructor", 
 			cascade={CascadeType.PERSIST, CascadeType.MERGE,
 					CascadeType.DETACH, CascadeType.REFRESH})
 	private List<Course> courses;
-	
+
 	public Instructor() {
 		
 	}
