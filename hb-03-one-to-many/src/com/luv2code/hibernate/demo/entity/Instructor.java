1170baa61a586c07acc0f469c1a859392b670eae
Nareloke <lucas_anielo@hotmail.com>
2021-03-22 11:21:53 -0300

    UP: 25.229 - @OneToMany Bi-Directional Update Instructor

--- hb-03-one-to-many/src/com/luv2code/hibernate/demo/entity/Instructor.java
+++ hb-03-one-to-many/src/com/luv2code/hibernate/demo/entity/Instructor.java
@@ -1,5 +1,8 @@
 package com.luv2code.hibernate.demo.entity;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
@@ -7,6 +10,7 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
+import javax.persistence.OneToMany;
 import javax.persistence.OneToOne;
 import javax.persistence.Table;
 
@@ -32,6 +36,11 @@ public class Instructor {
 	@JoinColumn(name="instructor_detail_id")
 	private InstructorDetail instructorDetail;
 
+	@OneToMany(mappedBy="instructor", 
+			cascade={CascadeType.PERSIST, CascadeType.MERGE,
+					CascadeType.DETACH, CascadeType.REFRESH})
+	private List<Course> courses;
+	
 	public Instructor() {
 		
 	}
@@ -82,9 +91,29 @@ public class Instructor {
 		this.instructorDetail = instructorDetail;
 	}
 
+	public List<Course> getCourses() {
+		return courses;
+	}
+
+	public void setCourses(List<Course> courses) {
+		this.courses = courses;
+	}
+
 	@Override
 	public String toString() {
 		return "Instructor [id=" + id + ", firstName=" + firstName + ", lastName=" + lastName + ", email=" + email
 				+ ", instructorDetail=" + instructorDetail + "]";
+	}
+
+	// add convenience methods for bi-directional relationship
+	public void add(Course tempCourse) {
+
+		if (courses == null) {
+			courses = new ArrayList<>();
+		}
+
+		courses.add(tempCourse);
+
+		tempCourse.setInstructor(this);
 	}
 }
