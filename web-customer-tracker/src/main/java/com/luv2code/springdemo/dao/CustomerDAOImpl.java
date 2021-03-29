a20d815ee56a977b9de2fe40776dca7d8a11399e
Nareloke <lucas_anielo@hotmail.com>
2021-03-29 13:30:02 -0300

    UP: 31.303 Add Customer. Save to Database II

--- web-customer-tracker/src/main/java/com/luv2code/springdemo/dao/CustomerDAOImpl.java
+++ web-customer-tracker/src/main/java/com/luv2code/springdemo/dao/CustomerDAOImpl.java
@@ -35,4 +35,14 @@ public class CustomerDAOImpl implements CustomerDAO {
 		// return the results
 		return customers;
 	}
+
+	@Override
+	public void saveCustomer(Customer theCustomer) {
+
+		// get current hibernate session
+		Session currentSession = sessionFactory.getCurrentSession();
+
+		// save the customer ...
+		currentSession.save(theCustomer);
+	}
 }
