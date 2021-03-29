c76c45ea75d0cc643e535d04ffc6d747c24b7e95
Nareloke <lucas_anielo@hotmail.com>
2021-03-29 15:07:46 -0300

    UP: 32.307 Update Customer. Prepopulating the Form I

--- web-customer-tracker/src/main/java/com/luv2code/springdemo/controller/CustomerController.java
+++ web-customer-tracker/src/main/java/com/luv2code/springdemo/controller/CustomerController.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import com.luv2code.springdemo.entity.Customer;
 import com.luv2code.springdemo.service.CustomerService;
@@ -58,5 +59,19 @@ public class CustomerController {
 		customerService.saveCustomer(theCustomer);
 
 		return "redirect:/customer/list";
+	}
+
+	@GetMapping("/showFormForUpdate")
+	public String showFormForUpdate(@RequestParam("customerId") int theId,
+										Model theModel)	 {
+
+		// get the customer from the database
+		Customer theCustomer = customerService.getCustomer(theid);
+
+		// set customer as a model attribute to pre-populate the form
+		theModel.addAttribute("customer", theCustomer);
+
+		// send over to our form
+		return "customer-form";
 	}
 }
