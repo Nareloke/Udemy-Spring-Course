5279330fc6e0ca6c28d373943380f043fdee4de0
Nareloke <lucas_anielo@hotmail.com>
2021-03-30 14:15:43 -0300

    CR: 35.326 | AOP: @Before Advice

---
+++ spring-demo-aop/src/com/luv2code/aopdemo/DemoConfig.java
@@ -0,0 +1,13 @@
+package com.luv2code.aopdemo;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
+import org.springframework.stereotype.Component;
+
+@Configuration
+@EnableAspectJAutoProxy
+@Component("com.luv2code.aopdemo")
+public class DemoConfig {
+
+	
+}
