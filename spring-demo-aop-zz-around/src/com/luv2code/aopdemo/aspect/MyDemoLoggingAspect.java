6f3bba9bc39d67665a0f30ee8279f77fc4c3f979
Nareloke <lucas_anielo@hotmail.com>
2021-03-31 17:32:37 -0300

    UP: 44.357 | AOP: @Around II

--- spring-demo-aop-zz-around/src/com/luv2code/aopdemo/aspect/MyDemoLoggingAspect.java
+++ spring-demo-aop-zz-around/src/com/luv2code/aopdemo/aspect/MyDemoLoggingAspect.java
@@ -3,9 +3,11 @@ package com.luv2code.aopdemo.aspect;
 import java.util.List;
 
 import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.After;
 import org.aspectj.lang.annotation.AfterReturning;
 import org.aspectj.lang.annotation.AfterThrowing;
+import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
 import org.aspectj.lang.reflect.MethodSignature;
@@ -107,6 +109,30 @@ public class MyDemoLoggingAspect {
 		// print out which method we are advising on
 		String method = theJoinPoint.getSignature().toShortString();
 		System.out.println("\n======>>>> Executing @After (finally) on method: " + method);
+	}
+
+	@Around("execution(* com.luv2code.aopdemo.service.*.getFortune(..))")
+	public Object aroundGetFortune(
+			ProceedingJoinPoint theProceedingJoinPoint) throws Throwable {
+
+		// print out method we are advising on
+		String method = theProceedingJoinPoint.getSignature().toShortString();
+		System.out.println("\n======>>>> Executing @Around on method: " + method);
+
+		// get begin timestamp
+		long begin = System.currentTimeMillis();
+
+		// now, let's execute the method
+		Object result = theProceedingJoinPoint.proceed();
+
+		// get end timestamp
+		long end = System.currentTimeMillis();
+
+		// compute duration and display it
+		long duration = end - begin;
+		System.out.println("\n======> Duration: " + duration / 1000.0 + " seconds");
+
+		return result;
 	}
 	
 }
