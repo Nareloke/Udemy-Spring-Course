4d7dfdb5895984098ffb67538f04ec7bcc172e73
Nareloke <lucas_anielo@hotmail.com>
2021-03-31 14:02:24 -0300

    UP: 40.344 | AOP: Read Method Arguments with JoinPoints II

--- spring-demo-aop-read-joinpoint/src/com/luv2code/aopdemo/MainDemoApp.java
+++ spring-demo-aop-read-joinpoint/src/com/luv2code/aopdemo/MainDemoApp.java
@@ -22,6 +22,9 @@ public class MainDemoApp {
 
 		// call the business method
 		Account myAccount = new Account();
+		myAccount.setName("Madhu");
+		myAccount.setLevel("Platinum");
+
 		theAccountDAO.addAccount(myAccount, true);
 		theAccountDAO.doWork();
 
--- spring-demo-aop-read-joinpoint/src/com/luv2code/aopdemo/aspect/MyDemoLoggingAspect.java
+++ spring-demo-aop-read-joinpoint/src/com/luv2code/aopdemo/aspect/MyDemoLoggingAspect.java
@@ -7,6 +7,8 @@ import org.aspectj.lang.reflect.MethodSignature;
 import org.springframework.core.annotation.Order;
 import org.springframework.stereotype.Component;
 
+import com.luv2code.aopdemo.Account;
+
 @Aspect
 @Component
 @Order(2)
@@ -26,6 +28,23 @@ public class MyDemoLoggingAspect {
 		System.out.println("Method: " + methodSig);
 
 		// display method arguments
+
+		// get args 
+		Object[] args = theJoinPoint.getArgs();
+
+		// loop through args
+		for (Object tempArg : args) {
+			System.out.println(tempArg);
+
+			if (tempArg instanceof Account) {
+
+				// downcast and print Account specific stuff
+				Account theAccount = (Account) tempArg;
+
+				System.out.println("account name: " + theAccount.getName());
+				System.out.println("account level: " + theAccount.getLevel());
+			}
+		}
 	}
 
 }
