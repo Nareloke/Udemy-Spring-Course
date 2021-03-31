97eceac4e39626221b4e7946420cbb3612f12ca0
Nareloke <lucas_anielo@hotmail.com>
2021-03-31 16:01:19 -0300

    CR: 42.350 | AOP: @AfterThrowing

---
+++ spring-demo-aop-z-after-throwing/src/com/luv2code/aopdemo/aspect/MyDemoLoggingAspect.java
@@ -0,0 +1,87 @@
+package com.luv2code.aopdemo.aspect;
+
+import java.util.List;
+
+import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.annotation.AfterReturning;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
+import org.aspectj.lang.reflect.MethodSignature;
+import org.springframework.core.annotation.Order;
+import org.springframework.stereotype.Component;
+
+import com.luv2code.aopdemo.Account;
+
+@Aspect
+@Component
+@Order(2)
+public class MyDemoLoggingAspect {
+
+	// this is where we add all of our related advices for logging
+
+	// let's start with an @Before advice
+
+	@Before("com.luv2code.aopdemo.aspect.LuvAopExpressions.forDaoPackageNoGetterSetter()")
+	public void beforeAddAccountAdvice(JoinPoint theJoinPoint) {
+
+		System.out.println("\n======>>> Executing @Before advice");
+
+		// display the method signature
+		MethodSignature methodSig = (MethodSignature) theJoinPoint.getSignature();
+		System.out.println("Method: " + methodSig);
+
+		// display method arguments
+
+		// get args 
+		Object[] args = theJoinPoint.getArgs();
+
+		// loop through args
+		for (Object tempArg : args) {
+			System.out.println(tempArg);
+
+			if (tempArg instanceof Account) {
+
+				// downcast and print Account specific stuff
+				Account theAccount = (Account) tempArg;
+
+				System.out.println("account name: " + theAccount.getName());
+				System.out.println("account level: " + theAccount.getLevel());
+			}
+		}
+	}
+
+	// add a new advice for @AfterReturning on the findAccounts method
+	@AfterReturning(
+			pointcut="execution(* com.luv2code.aopdemo.dao.AccountDAO.findAccounts(..))",
+			returning="result"
+			)
+	public void afterReturningFindAccountsAdvice(
+			JoinPoint theJoinPoint, List<Account> result) {
+
+		// print out which method we are advising on
+		String method = theJoinPoint.getSignature().toShortString();
+		System.out.println("\n======>>>> Executing @AfterReturning on method: " + method);
+
+		// print out the results of the method call
+		System.out.println("\n======>>>> Result is: " + result);
+
+		// let's post-process the data ... let's modify it
+
+		// convert the account names to uppercase
+		convertAccountNamesToUpperCase(result);
+	}
+
+	private void convertAccountNamesToUpperCase(List<Account> result) {
+
+		// loop through accounts
+		for (Account tempAccount : result) {
+
+			// get uppercase version of name
+			String theUpperName = tempAccount.getName().toUpperCase();
+
+			// update the name of the account
+			tempAccount.setName(theUpperName);
+		}
+	}
+
+}
