f676086f24f76a81682dab56245b7be7a1f5d330
Nareloke <lucas_anielo@hotmail.com>
2021-03-30 21:11:05 -0300

    UP: 38.339 | AOP: Combining Pointcuts - Write Some Code II

--- spring-demo-aop-pointcut-declarations-combo/src/com/luv2code/aopdemo/aspect/MyDemoLoggingAspect.java
+++ spring-demo-aop-pointcut-declarations-combo/src/com/luv2code/aopdemo/aspect/MyDemoLoggingAspect.java
@@ -15,14 +15,26 @@ public class MyDemoLoggingAspect {
 
 	@Pointcut("execution(* com.luv2code.aopdemo.dao.*.*(..))")
 	public void forDaoPackage() {}
-	
-	@Before("forDaoPackage()")
+
+	// create pointcut for getter methods
+	@Pointcut("execution(* com.luv2code.aopdemo.dao.*.get*(..))")
+	private void getter() {}
+
+	// create pointcut for setter methods
+	@Pointcut("execution(* com.luv2code.aopdemo.dao.*.set*(..))")
+	private void setter() {}
+
+	// create point: include package ... exclude getter/setter
+	@Pointcut("forDaoPackage() && !(getter() || setter())")
+	private void forDaoPackageNoGetterSetter() {}
+
+	@Before("forDaoPackageNoGetterSetter()")
 	public void beforeAddAccountAdvice() {
 
 		System.out.println("\n======>>> Executing @Before advice");
 	}
 
-	@Before("forDaoPackage()")
+	@Before("forDaoPackageNoGetterSetter()")
 	public void performApiAnalytics() {
 
 		System.out.println("\n======>>> Performing API analytics");
