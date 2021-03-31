8bda591faf547231451a2ba4e5307e7ff2522918
Nareloke <lucas_anielo@hotmail.com>
2021-03-31 13:42:30 -0300

    CR: 40.343 | AOP: Read Method Arguments with JoinPoints I

---
+++ spring-demo-aop-read-joinpoint/src/com/luv2code/aopdemo/aspect/MyDemoLoggingAspect.java
@@ -0,0 +1,31 @@
+package com.luv2code.aopdemo.aspect;
+
+import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
+import org.aspectj.lang.reflect.MethodSignature;
+import org.springframework.core.annotation.Order;
+import org.springframework.stereotype.Component;
+
+@Aspect
+@Component
+@Order(2)
+public class MyDemoLoggingAspect {
+
+	// this is where we add all of our related advices for logging
+
+	// let's start with an @Before advice
+
+	@Before("com.luv2code.aopdemo.aspect.LuvAopExpressions.forDaoPackageNoGetterSetter()")
+	public void beforeAddAccountAdvice(JoinPoint theJoinPoint) {
+
+		System.out.println("\n======>>> Executing @Before advice");
+
+		// display the method signature
+		MethodSignature methodSig = (MethodSignature) theJoinPoint.getSignature();
+		System.out.println("Method: " + methodSig);
+
+		// display method arguments
+	}
+
+}
