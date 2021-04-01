56af762a160f89da59b53fb27423b8b458eac3c9
Nareloke <lucas_anielo@hotmail.com>
2021-04-01 13:19:00 -0300

    UP: 45.365 | AOP: AOP and Spring MVC App - Add @AfterReturning Advice

--- web-customer-tracker-aop/src/main/java/com/luv2code/springdemo/aspect/CRMLoggingAspect.java
+++ web-customer-tracker-aop/src/main/java/com/luv2code/springdemo/aspect/CRMLoggingAspect.java
@@ -3,6 +3,7 @@ package com.luv2code.springdemo.aspect;
 import java.util.logging.Logger;
 
 import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.annotation.AfterReturning;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
 import org.aspectj.lang.annotation.Pointcut;
@@ -38,8 +39,28 @@ public class CRMLoggingAspect {
 		myLogger.info("=====>>>> in @Before: calling method: " + theMethod);
 
 		// display the arguments to the method
+
+		// get the arguments
+		Object[] args= theJoinPoint.getArgs();
+
+		// loop thru and display args
+		for (Object tempArg : args) {
+			myLogger.info("=====>>>> argument: " + tempArg);
+		}
 	}
 
 	// add @AfterReturning advice
+	@AfterReturning(
+			pointcut="forAppFlow()",
+			returning="theResult"
+			)
+	public void afterReturning(JoinPoint theJoinPoint, Object theResult) {
+
+		String theMethod = theJoinPoint.getSignature().toShortString();
+		myLogger.info("=====>>>> in @AfterReturning: from method: " + theMethod);
+
+		// display data returned
+		myLogger.info("=====>> result: " + theResult);
+	}
 
 }
