3f17a7241a83f5374de081e57f6c3ee32543a937
Nareloke <lucas_anielo@hotmail.com>
2021-04-01 18:00:20 -0300

    CR: 47.394 | Spring Security - Create Security Initializer

---
+++ spring-security-demo-02-base-security/src/main/java/com/luv2code/springsecurity/demo/config/DemoSecurityConfig.java
@@ -0,0 +1,18 @@
+package com.luv2code.springsecurity.demo.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+@EnableWebSecurity
+public class DemoSecurityConfig extends WebSecurityConfigurerAdapter {
+
+	@Override
+	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+		// TODO Auto-generated method stub
+		super.configure(auth);
+	}
+	
+}
