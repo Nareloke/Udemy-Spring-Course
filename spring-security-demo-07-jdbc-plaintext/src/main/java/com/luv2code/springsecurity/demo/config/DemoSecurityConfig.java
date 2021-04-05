cd8c4242bfbf68e8a5a774b32cc6e0ef5a49d8ab
Nareloke <lucas_anielo@hotmail.com>
2021-04-05 16:42:41 -0300

    UP: 54.441 | Spring Security JDBC - Add  JDBC Authentication

--- spring-security-demo-07-jdbc-plaintext/src/main/java/com/luv2code/springsecurity/demo/config/DemoSecurityConfig.java
+++ spring-security-demo-07-jdbc-plaintext/src/main/java/com/luv2code/springsecurity/demo/config/DemoSecurityConfig.java
@@ -1,45 +1,45 @@
 package com.luv2code.springsecurity.demo.config;
 
+import javax.sql.DataSource;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.User.UserBuilder;
 
 @Configuration
 @EnableWebSecurity
 public class DemoSecurityConfig extends WebSecurityConfigurerAdapter {
 
+	@Autowired
+	private DataSource securityDataSource;
+
 	@Override
 	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
 
-		// add our users for in memory authentication
-		UserBuilder users = User.withDefaultPasswordEncoder();
-		auth.inMemoryAuthentication()
-			.withUser(users.username("john").password("test123").roles("EMPLOYEE"))
-			.withUser(users.username("mary").password("test123").roles("MANAGER","EMPLOYEE"))
-			.withUser(users.username("susan").password("test123").roles("ADMIN", "EMPLOYEE"));
+		// use jdbc authentication
+		auth.jdbcAuthentication().dataSource(securityDataSource);
 	}
 
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
 
 		http.authorizeRequests()
-				// .anyRequest().authenticated() // Any request to the app must be authenticated
-				.antMatchers("/").hasRole("EMPLOYEE")
-				.antMatchers("/leaders/**").hasRole("MANAGER")
-				.antMatchers("/systems/**").hasRole("ADMIN")
-			.and()
-			.formLogin() // Customizing the form login process
-				.loginPage("/showMyLoginPage") // Show our custom form at the request mapping
-				.loginProcessingUrl("/authenticateTheUser")
-				.permitAll()
-			.and()
-			.logout().permitAll() // Adds logout support
-			.and()
-			.exceptionHandling().accessDeniedPage("/access-denied"); // custom path for access denied pages
+		// .anyRequest().authenticated() // Any request to the app must be authenticated
+		.antMatchers("/").hasRole("EMPLOYEE")
+		.antMatchers("/leaders/**").hasRole("MANAGER")
+		.antMatchers("/systems/**").hasRole("ADMIN")
+		.and()
+		.formLogin() // Customizing the form login process
+		.loginPage("/showMyLoginPage") // Show our custom form at the request mapping
+		.loginProcessingUrl("/authenticateTheUser")
+		.permitAll()
+		.and()
+		.logout().permitAll() // Adds logout support
+		.and()
+		.exceptionHandling().accessDeniedPage("/access-denied"); // custom path for access denied pages
 	}
 
 }
