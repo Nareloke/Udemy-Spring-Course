ba335d19e3c91ce39e4de8fbf9d81990d85df111
Nareloke <lucas_anielo@hotmail.com>
2021-04-05 16:02:40 -0300

    UP: 54.438 | Spring Security JDBC - Coding - Define DataSource

--- spring-security-demo-07-jdbc-plaintext/src/main/java/com/luv2code/springsecurity/demo/config/DemoAppConfig.java
+++ spring-security-demo-07-jdbc-plaintext/src/main/java/com/luv2code/springsecurity/demo/config/DemoAppConfig.java
@@ -1,8 +1,15 @@
 package com.luv2code.springsecurity.demo.config;
 
+import java.util.logging.Logger;
+
+import javax.sql.DataSource;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.core.env.Environment;
 import org.springframework.web.servlet.ViewResolver;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.view.InternalResourceViewResolver;
@@ -10,7 +17,15 @@ import org.springframework.web.servlet.view.InternalResourceViewResolver;
 @Configuration
 @EnableWebMvc
 @ComponentScan(basePackages="com.luv2code.springsecurity.demo")
+@PropertySource("classpath:persistence-mysql.properties")
 public class DemoAppConfig {
+
+	// set up variable to hold the properties
+	@Autowired
+	private Environment env; // Will hold data read from properties files
+
+	// set up a logger for diagnostics
+	private Logger logger = Logger.getLogger(getClass().getName());
 
 	// define a bean for ViewResolver
 	@Bean
@@ -20,6 +35,21 @@ public class DemoAppConfig {
 		viewResolver.setPrefix("/WEB-INF/view/");
 		viewResolver.setSuffix(".jsp");
 		return viewResolver;
+	}
+
+	// define a bean for our security datasource
+	@Bean
+	public DataSource securityDataSource() {
+
+		// create connection pool
+
+		// set the jdbc driver class
+
+		// log the connection props
+
+		// set connection pool props
+
+		return null;
 	}
 
 }
