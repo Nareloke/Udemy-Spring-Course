4bea08a8c82b219db0ecda8d1822b4ee5515c7e7
Nareloke <lucas_anielo@hotmail.com>
2021-04-05 21:41:56 -0300

    CR&UP: 57.459 Spring REST - JSON Jackson Demo - Nested Objects and Arrays

--- jackson-databinding-json-demo/src/main/java/com/luv2code/jackson/json/demo/Driver.java
+++ jackson-databinding-json-demo/src/main/java/com/luv2code/jackson/json/demo/Driver.java
@@ -15,7 +15,7 @@ public class Driver {
 			// read JSON file and map/convert to Java POJO:
 			// data/sample-lite.json
 			Student theStudent =
-					mapper.readValue(new File("data/sample-lite.json"), Student.class);
+					mapper.readValue(new File("data/sample-full.json"), Student.class);
 
 			// print first name and last name
 			System.out.println("First name = " + theStudent.getFirstName());
