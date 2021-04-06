57b7cc2ef05e71f465c5a6926823bbad6d0cdd0b
Nareloke <lucas_anielo@hotmail.com>
2021-04-06 16:27:04 -0300

    UP: 60.475 | Spring REST - Using @PathVariable for REST Endpoints

--- spring-rest-demo/src/main/java/com/luv2code/springdemo/rest/StudentRestController.java
+++ spring-rest-demo/src/main/java/com/luv2code/springdemo/rest/StudentRestController.java
@@ -3,6 +3,8 @@ package com.luv2code.springdemo.rest;
 import java.util.ArrayList;
 import java.util.List;
 
+import javax.annotation.PostConstruct;
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -13,15 +15,22 @@ import com.luv2code.springdemo.entity.Student;
 @RequestMapping("/api")
 public class StudentRestController {
 
-	// define endpoint for "/students" - return list of students
-	@GetMapping("/students")
-	public List<Student> getStudents() {
+	private List<Student> theStudents;
 
-		List<Student> theStudents = new ArrayList<>();
+	// define PostConstruct to load the student data ... only once!
+	@PostConstruct
+	public void loadData() {
+
+		theStudents = new ArrayList<>();
 
 		theStudents.add(new Student("Poornima", "Patel"));
 		theStudents.add(new Student("Mario", "Rossi"));
 		theStudents.add(new Student("Mary", "Smith"));
+	}
+
+	// define endpoint for "/students" - return list of students
+	@GetMapping("/students")
+	public List<Student> getStudents() {
 
 		return theStudents;
 	}
