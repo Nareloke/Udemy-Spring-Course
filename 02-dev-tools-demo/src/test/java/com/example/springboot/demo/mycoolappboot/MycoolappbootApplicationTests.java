698e61268ad8e803379b221d8bfcf626d5bd1063
Nareloke <lucas_anielo@hotmail.com>
2021-04-07 17:31:08 -0300

    CR: 71.520 | Spring Boot - Dev Tools

---
+++ 02-dev-tools-demo/src/test/java/com/example/springboot/demo/mycoolappboot/MycoolappbootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springboot.demo.mycoolappboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MycoolappbootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
