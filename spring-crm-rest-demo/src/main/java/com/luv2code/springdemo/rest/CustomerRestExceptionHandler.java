b877073da391a927be75dd59f39ca011098fdbda
Nareloke <lucas_anielo@hotmail.com>
2021-04-07 13:48:33 -0300

    CR&UP: 65.499 | Spring REST - CRUD Database - Exception Handling - Refactor

---
+++ spring-crm-rest-demo/src/main/java/com/luv2code/springdemo/rest/CustomerRestExceptionHandler.java
@@ -0,0 +1,11 @@
+package com.luv2code.springdemo.rest;
+
+import org.springframework.web.bind.annotation.ControllerAdvice;
+
+@ControllerAdvice
+public class CustomerRestExceptionHandler {
+
+	// Add an exception handler for CustomerNotFoundException
+
+	// Add another exception handler .. to catch any exception (catch all)
+}
