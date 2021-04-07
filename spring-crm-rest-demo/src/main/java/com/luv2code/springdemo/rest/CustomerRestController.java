7c075ec474784dbf843a5eb2aa95c9c7e3a88ed7
Nareloke <lucas_anielo@hotmail.com>
2021-04-07 13:47:37 -0300

    UP: 65.498 | Spring REST - CRUD Database - Exception Handling - Responses

--- spring-crm-rest-demo/src/main/java/com/luv2code/springdemo/rest/CustomerRestController.java
+++ spring-crm-rest-demo/src/main/java/com/luv2code/springdemo/rest/CustomerRestController.java
@@ -31,6 +31,11 @@ public class CustomerRestController {
 	public Customer getCustomer(@PathVariable int customerId) {
 
 		Customer theCustomer = customerService.getCustomer(customerId);
+
+		if (theCustomer == null) {
+			throw new CustomerNotFoundException("Customer id not found - " + customerId);
+		}
+
 		return theCustomer;
 	}
 }
