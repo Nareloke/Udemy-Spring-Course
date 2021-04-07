f2add239399b1ac23084e52698b7118fc009e236
Nareloke <lucas_anielo@hotmail.com>
2021-04-07 14:03:32 -0300

    UP: 65.501 | Spring REST - CRUD Database - Exception Handling - Catch All

--- spring-crm-rest-demo/src/main/java/com/luv2code/springdemo/rest/CustomerRestExceptionHandler.java
+++ spring-crm-rest-demo/src/main/java/com/luv2code/springdemo/rest/CustomerRestExceptionHandler.java
@@ -24,5 +24,18 @@ public class CustomerRestExceptionHandler {
 	}
 
 	// Add another exception handler .. to catch any exception (catch all)
+	@ExceptionHandler
+	public ResponseEntity<CustomerErrorResponse> handleException(
+			Exception exc) {
+
+		// create CustomerErrorResponse
+		CustomerErrorResponse error = new CustomerErrorResponse(
+				HttpStatus.BAD_REQUEST.value(),
+				exc.getMessage(),
+				System.currentTimeMillis());
+
+		// return ResponseEntity
+		return new ResponseEntity<>(error, HttpStatus.BAD_REQUEST);
+	}
 
 }
