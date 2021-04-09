1593e3a215961db50d295319788fefe2c1bda65d
Nareloke <lucas_anielo@hotmail.com>
2021-04-09 16:46:12 -0300

    UP: 74.536 | Spring Boot - REST Controller Methods to Find and Add Employee

--- cruddemo/src/main/java/com/example/springboot/cruddemo/rest/EmployeeRestController.java
+++ cruddemo/src/main/java/com/example/springboot/cruddemo/rest/EmployeeRestController.java
@@ -5,6 +5,9 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -27,6 +30,30 @@ public class EmployeeRestController {
 	public List<Employee> findAll() {
 
 		return employeeService.findAll();
+	}
+
+	@GetMapping("/employees/{employeeId}")
+	public Employee getEmployee(@PathVariable int employeeId) {
+
+		Employee theEmployee = employeeService.findById(employeeId);
+
+		if (theEmployee == null) {
+			throw new RuntimeException("Employee id not found - " + employeeId);
+		}
+
+		return theEmployee;
+	}
+
+	@PostMapping("/employees")
+	public Employee addEmployee(@RequestBody Employee theEmployee) {
+
+		// also just in case they pass an id in JSON ... set id to 0
+		// this is to force a save of new item ... instead of update
+		theEmployee.setId(0);
+
+		employeeService.save(theEmployee);
+
+		return theEmployee;
 	}
 
 }
