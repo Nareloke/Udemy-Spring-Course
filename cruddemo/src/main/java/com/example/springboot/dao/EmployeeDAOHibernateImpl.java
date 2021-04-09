d8e99419c72095052985cafb1b151b8a26fcbbe4
Nareloke <lucas_anielo@hotmail.com>
2021-04-09 13:17:00 -0300

    UP: 74.534 | Spring Boot - Additional CRUD Methods for DAO

--- cruddemo/src/main/java/com/example/springboot/dao/EmployeeDAOHibernateImpl.java
+++ cruddemo/src/main/java/com/example/springboot/dao/EmployeeDAOHibernateImpl.java
@@ -40,4 +40,40 @@ public class EmployeeDAOHibernateImpl implements EmployeeDAO {
 		return employees;
 	}
 
+	@Override
+	public Employee findById(int theId) {
+
+		// get the current hibernate session
+		Session currentSession = entityManager.unwrap(Session.class);
+
+		// get the employee
+		Employee theEmployee =
+				currentSession.get(Employee.class, theId);
+
+		// return the employee
+		return theEmployee;
+	}
+
+	@Override
+	public void save(Employee theEmployee) {
+
+		// get the current hibernate session
+		Session currentSession = entityManager.unwrap(Session.class);
+
+		// save employee
+		currentSession.saveOrUpdate(theEmployee);
+	}
+
+	@Override
+	public void deleteById(int theId) {
+
+		// get the current hibernate session
+		Session currentSession = entityManager.unwrap(Session.class);
+
+		// delete object with primary key
+		Query theQuery =
+				currentSession.createQuery(
+						"DELETE FROM Employee WHERE id = :employeeId");
+	}
+
 }
