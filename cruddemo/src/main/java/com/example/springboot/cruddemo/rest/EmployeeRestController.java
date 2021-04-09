1422f9a917018c85905506eb12186af116f5bb54
Nareloke <lucas_anielo@hotmail.com>
2021-04-09 17:00:01 -0300

    UP: 74.537 | Spring Boot - REST Controller Methods to Update and Delete an Employee

--- cruddemo/src/main/java/com/example/springboot/cruddemo/rest/EmployeeRestController.java
+++ cruddemo/src/main/java/com/example/springboot/cruddemo/rest/EmployeeRestController.java
@@ -4,9 +4,11 @@ package com.example.springboot.cruddemo.rest;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -56,4 +58,26 @@ public class EmployeeRestController {
 		return theEmployee;
 	}
 
+	@PutMapping("/employees")
+	public Employee updateEmployee(@RequestBody Employee theEmployee) {
+
+		employeeService.save(theEmployee);
+
+		return theEmployee;
+	}
+
+	@DeleteMapping("/employees/{employeeId}")
+	public String deleteEmployee(@PathVariable int employeeId) {
+
+		Employee tempEmployee = employeeService.findById(employeeId);
+
+		// throw exception if null
+		if (tempEmployee == null) {
+			throw new RuntimeException("Employee id not found - " + employeeId);
+		}
+
+		employeeService.deleteById(employeeId);
+
+		return "Delete employee id -" + employeeId;
+	}
 }
