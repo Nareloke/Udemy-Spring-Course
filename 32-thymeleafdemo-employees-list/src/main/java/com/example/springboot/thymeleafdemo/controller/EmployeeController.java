234c3cbe034964c56d870c34ffa23cf9afc669c9
Nareloke <lucas_anielo@hotmail.com>
2021-04-09 22:09:49 -0300

    CR: 79.551 | Spring Boot - Thymeleaf - Build HTML Tables II

--- 32-thymeleafdemo-employees-list/src/main/java/com/example/springboot/thymeleafdemo/controller/EmployeeController.java
+++ 32-thymeleafdemo-employees-list/src/main/java/com/example/springboot/thymeleafdemo/controller/EmployeeController.java
@@ -6,6 +6,8 @@ import java.util.List;
 import javax.annotation.PostConstruct;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.example.springboot.thymeleafdemo.model.Employee;
@@ -15,7 +17,7 @@ import com.example.springboot.thymeleafdemo.model.Employee;
 public class EmployeeController {
 
 	// load employee data
-	private List<Employee> theEmployee;
+	private List<Employee> theEmployees;
 
 	@PostConstruct
 	private void loadData() {
@@ -26,14 +28,22 @@ public class EmployeeController {
 		Employee emp3 = new Employee(3, "Avani", "Gupta", "avani@gmail.com");
 
 		// create the list
-		theEmployee = new ArrayList<>();
+		theEmployees = new ArrayList<>();
 
 		// add to the list
-		theEmployee.add(emp1);
-		theEmployee.add(emp2);
-		theEmployee.add(emp3);
+		theEmployees.add(emp1);
+		theEmployees.add(emp2);
+		theEmployees.add(emp3);
 	}
 
 	// add mapping for "/list"
+	@GetMapping("/list")
+	public String listEmployees(Model theModel) {
+
+		// add to the spring model
+		theModel.addAttribute("employees", theEmployees);
+
+		return "list-employees";
+	}
 
 }
