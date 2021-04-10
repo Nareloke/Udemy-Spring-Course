5f979fe7a7d9c1eace0e8d379522b41b3ad28707
Nareloke <lucas_anielo@hotmail.com>
2021-04-09 23:17:51 -0300

    CR: 80.556 Spring Boot - Thymeleaf - CRUD Database Project: Add Employee III

---
+++ 35-thymeleafdemo-employees-add/src/main/java/com/example/springboot/thymeleafdemo/service/EmployeeServiceImpl.java
@@ -0,0 +1,59 @@
+package com.example.springboot.thymeleafdemo.service;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.example.springboot.thymeleafdemo.dao.EmployeeRepository;
+import com.example.springboot.thymeleafdemo.entity.Employee;
+
+@Service
+public class EmployeeServiceImpl implements EmployeeService {
+
+	private EmployeeRepository employeeRepository;
+
+	@Autowired
+	public EmployeeServiceImpl(EmployeeRepository theEmployeeRepository) {
+
+		employeeRepository = theEmployeeRepository;
+	}
+
+	@Override
+	@Transactional
+	public List<Employee> findAll() {
+
+		return employeeRepository.findAllByOrderByLastNameAsc();
+	}
+
+	@Override
+	public Employee findById(int theId) {
+
+		Optional<Employee> result = employeeRepository.findById(theId);
+
+		Employee theEmployee = null;
+		if (result.isPresent()) {
+			theEmployee = result.get();
+		} else {
+			// we didn't find the employee
+			throw new RuntimeException("Did not find employee id - " + theId);
+		}
+
+		return theEmployee;
+	}
+
+	@Override
+	public void save(Employee theEmployee) {
+
+		employeeRepository.save(theEmployee);
+	}
+
+	@Override
+	public void deleteById(int theId) {
+
+		employeeRepository.deleteById(theId);
+	}
+
+}
